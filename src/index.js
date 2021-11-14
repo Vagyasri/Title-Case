49c618e563fcf44f270229b976fd8eb406f9c965
Vagyasri <vagyasri2011@gmail.com>
2021-11-14 23:08:21 +0100

    Update JS File

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+
+function titleCase(str) {
+    let arr = str.toLowerCase().split(' ');
+    let newArr =[];
+    for(let i =0; i < arr.length; i++) {
+    newArr[i] = arr[i].replace(arr[i].charAt(0), arr[i].charAt(0).toUpperCase());
+    }
+    return newArr.join(' ');
+  }
+  
+  console.log(titleCase("I'm a little tea pot")); // I'm A Little Tea Pot
+  console.log(titleCase("sHoRt AnD sToUt")); // Short And Stout
+  console.log(titleCase("HERE IS MY HANDLE HERE IS MY SPOUT")); // Here Is My Handle Here Is My Spout
